a4ee22a16d5b43494e8b905fa62657c5f85f792c
TexHaPb85 <16fi.m.yevtushenko@std.npu.edu.ua>
2019-07-07 17:21:03 +0300

    chnges test

--- src/main/java/entities/Hall.java
+++ src/main/java/entities/Hall.java
@@ -8,4 +8,34 @@ public class Hall {
     private TypeOfPlace typeOfPlace;  //type of place(normal, premium, vip),
 
 
+    public Hall(String number, int amountOfPlaces, TypeOfPlace typeOfPlace) {
+        this.number = number;
+        this.amountOfPlaces = amountOfPlaces;
+        this.typeOfPlace = typeOfPlace;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public int getAmountOfPlaces() {
+        return amountOfPlaces;
+    }
+
+    public void setAmountOfPlaces(int amountOfPlaces) {
+        this.amountOfPlaces = amountOfPlaces;
+    }
+
+    public TypeOfPlace getTypeOfPlace() {
+        return typeOfPlace;
+    }
+
+    public void setTypeOfPlace(TypeOfPlace typeOfPlace) {
+        this.typeOfPlace = typeOfPlace;
+    }
 }
+
